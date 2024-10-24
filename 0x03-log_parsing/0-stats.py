ba1702d4eaec11570c5865b8f2f74c5147123ee0
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-25 00:05:00 +0300

    regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,14 @@
 A script that reads stdin line by
 line and computes metrics.
 """
-from sys import stdin
+import sys
+import re
+import signal
+
+
+def handler(signum, frame):
+    print_stats(total_size, status_codes)
+    sys.exit(0)
 
 
 def print_stats(total_size: int, codes_counts: dict) -> None:
@@ -15,6 +22,13 @@ def print_stats(total_size: int, codes_counts: dict) -> None:
 
 
 if __name__ == "__main__":
+    log_pattern = re.compile(
+        r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3}) - '
+        r'\[(?P<date>.*?)\] '
+        r'"GET /projects/260 HTTP/1.1" '
+        r'(?P<status>\d{3}) '
+        r'(?P<size>\d+)'
+        )
     total_size = 0
     status_codes = {200: 0,
                     301: 0,
@@ -26,24 +40,22 @@ if __name__ == "__main__":
                     500: 0}
     line_count = 0
 
-    try:
-        for line in stdin:
-            line_count += 1
-            parts = line.split()
+    signal.signal(signal.SIGINT, handler)
+    for line in sys.stdin:
+        line_count += 1
+        match = log_pattern.match(line)
+        if not match:
+            continue
 
-            try:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
-                total_size += file_size
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
+        try:
+            status_code = int(match.group('status'))
+            file_size = int(match.group('size'))
+            total_size += file_size
+            if status_code in status_codes:
+                status_codes[status_code] += 1
 
-            except (ValueError, IndexError):
-                continue
-            if line_count == 10:
-                line_count = 0
-                print_stats(total_size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
+        except (ValueError, IndexError):
+            continue
+        if line_count == 10:
+            line_count = 0
+            print_stats(total_size, status_codes)
