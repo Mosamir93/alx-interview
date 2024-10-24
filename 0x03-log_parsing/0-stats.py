9593e2c179bfe04e3db5e143016f0208ba7d3755
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-25 00:51:58 +0300

    formatting2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,37 @@
 #!/usr/bin/python3
-"""
-A script that reads stdin line by
-line and computes metrics.
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
-import re
 
-
-log_pattern = re.compile(
-    r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3}) - '
-    r'\[(?P<date>.*?)\] '
-    r'"GET /projects/260 HTTP/1.1" '
-    r'(?P<status>\d{3}) '
-    r'(?P<size>\d+)'
-    )
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_codes = {200: 0,
-                301: 0,
-                400: 0,
-                401: 0,
-                403: 0,
-                404: 0,
-                405: 0,
-                500: 0}
-line_count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        line_count += 1
-        match = log_pattern.match(line)
-        if not match:
-            continue
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        try:
-            status_code = int(match.group('status'))
-            file_size = int(match.group('size'))
-            total_size += file_size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        except (ValueError, IndexError):
-            continue
-        if line_count == 10:
-            line_count = 0
-            print(f"File size: {total_size}")
-            for key, value in sorted(status_codes.items()):
-                if value > 0:
-                    print(f"{key}: {value}")
-except KeyboardInterrupt:
-    print(f"File size: {total_size}")
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print(f"{key}: {value}")
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
