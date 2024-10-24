8844e373f59b3ae627e2dff17198dd00023a8e66
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-25 01:14:58 +0300

    removing regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,16 +4,8 @@ A script that reads stdin line by
 line and computes metrics.
 """
 import sys
-import re
 
 
-log_pattern = re.compile(
-    r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3}) - '
-    r'\[(?P<date>.*?)\] '
-    r'"GET /projects/260 HTTP/1.1" '
-    r'(?P<status>\d{3}) '
-    r'(?P<size>\d+)'
-    )
 total_size = 0
 status_codes = {'200': 0,
                 '301': 0,
@@ -27,26 +19,24 @@ line_count = 0
 
 try:
     for line in sys.stdin:
-        match = log_pattern.match(line)
-        if not match:
-            continue
+        line.split()
+        if len(line) > 4:
+            try:
+                status_code = int(match.group('status'))
+                file_size = int(match.group('size'))
+                line_count += 1
+                total_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
 
-        try:
-            status_code = int(match.group('status'))
-            file_size = int(match.group('size'))
-            line_count += 1
-            total_size += file_size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-
-        except (ValueError, IndexError):
-            continue
-        if line_count == 10:
-            line_count = 0
-            print("File size: {}".format(total_size))
-            for key, value in sorted(status_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
+            except (ValueError, IndexError):
+                continue
+            if line_count == 10:
+                line_count = 0
+                print("File size: {}".format(total_size))
+                for key, value in sorted(status_codes.items()):
+                    if value > 0:
+                        print("{}: {}".format(key, value))
 except KeyboardInterrupt:
     print("File size: {}".format(total_size))
     for key, value in sorted(status_codes.items()):
